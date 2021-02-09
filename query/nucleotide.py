82b75bf5f1d2b6f5f6c7987a6300613c9d83e732
Victor Lin <victor@victorl.in>
2021-02-09 11:26:24 -0500

    implement simple export

--- query/nucleotide.py
+++ query/nucleotide.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy_caching import FromCache
 from model.nucleotide import nsra, nfamily, nsequence
 from . import apply_filters
 from application import cache
+import math
 
 
 # sra
@@ -29,6 +30,20 @@ def get_family_pagination(family, page=1, perPage=20, **kwargs):
         .options(FromCache(cache)))
     query = apply_filters(query, nfamily, **kwargs)
     return query.paginate(page, perPage)
+
+def get_family_export(family, **kwargs):
+    result = []
+    numPages = float('inf')  # set by first pagination
+    perPage = 1000  # Aurora Data API limit
+    for page in range(1,5):
+        pagination = get_family_pagination(family, page, perPage, **kwargs)
+        result.extend(pagination.items)
+        if page == 1:
+            numPages = math.ceil(pagination.total / perPage)
+        print(f'{page} / {numPages}')
+        if page >= numPages:
+            break
+    return result
 
 # genbank
 
