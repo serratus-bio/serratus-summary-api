b9a55955154edc2cd95511f3e822d2c57b979ab7
Victor Lin <victor@victorl.in>
2021-02-09 12:27:56 -0500

    add test

--- tests/test_export.py
+++ tests/test_export.py
@@ -11,3 +11,6 @@ def test_family_export():
 
     result = get_family_export('Coronaviridae', **{'scoreMin': 50})
     assert (len(result) == 3637)
+
+    result = get_family_export('Coronaviridae', **{'scoreMin': 100, 'scoreMax': 50})
+    assert (len(result) == 0)
