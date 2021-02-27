e5ebc019ff592265c689ed80f126d19836ae0e59
Victor Lin <victor@victorl.in>
2021-02-27 12:09:27 -0500

    reuse code

--- query/nucleotide.py
+++ query/nucleotide.py
@@ -20,21 +20,22 @@ def get_sra_sequences(sra):
 
 # matches
 
+# url param key : table model
 tableMap = {
     'family': nfamily,
     'genbank': nsequence
 }
 
-def get_matches(**url_params):
-    if 'family' in url_params:
-        key = 'family'
-    elif 'genbank' in url_params:
-        key = 'genbank'
+def get_table_key(**url_params):
+    for key in tableMap:
+        if key in url_params:
+            return key
 
+def get_matches(**url_params):
+    key = get_table_key(**url_params)
     value = url_params.pop(key)
     table = tableMap[key]
     filter_col = getattr(table, table.filter_col_name)
-
     query = (table.query
         .filter(filter_col == value)
         .with_entities(table.sra_id)
@@ -44,20 +45,13 @@ def get_matches(**url_params):
     return sra_ids
 
 def get_matches_paginated(page=1, perPage=20, **url_params):
-    page = int(page)
-    perPage = int(perPage)
-    if 'family' in url_params:
-        key = 'family'
-    elif 'genbank' in url_params:
-        key = 'genbank'
-
+    key = get_table_key(**url_params)
     value = url_params.pop(key)
     table = tableMap[key]
     filter_col = getattr(table, table.filter_col_name)
-
     query = (table.query
         .filter(filter_col == value)
         .order_by(table.score.desc())
         .options(FromCache(cache)))
     query = apply_filters(query, table, **url_params)
-    return query.paginate(page, perPage)
+    return query.paginate(int(page), int(perPage))
