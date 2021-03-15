f691f76b0b047ae9e213ce1d4542b0f0cabcdf56
Victor Lin <victor@victorl.in>
2021-03-14 23:48:36 -0400

    use QueryBase/NucleotideQuery

--- query/__init__.py
+++ query/__init__.py
@@ -1,10 +1,2 @@
-def apply_filters(query, model, scoreMin=None, scoreMax=None, identityMin=None, identityMax=None):
-    if scoreMin:
-        query = query.filter(model.score >= int(scoreMin))
-    if scoreMax:
-        query = query.filter(model.score <= int(scoreMax))
-    if identityMin:
-        query = query.filter(model.percent_identity >= int(identityMin))
-    if identityMax:
-        query = query.filter(model.percent_identity <= int(identityMax))
-    return query
+from .nucleotide import NucleotideQuery
+nucleotide_query = NucleotideQuery()
