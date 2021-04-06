cf5ed0a5b22a29ee21501c5b77985b2d3301b13d
Victor Lin <victor@victorl.in>
2021-04-06 12:06:54 -0400

    use new cluster, table -> view

--- query/sra.py
+++ query/sra.py
@@ -1,5 +1,5 @@
 from .base import QueryBase
-from model.tables.sra import analysis_index
+from model.views.sra import analysis_index
 
 def get_analysis_index(run_id):
     query = analysis_index.query.filter(analysis_index.run_id == run_id)
