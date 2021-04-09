ccaed38bb5f8b6dc548b485ec89a3939b0083bf8
Victor Lin <victor@victorl.in>
2021-04-09 10:45:51 -0400

    use route-based tests for rdrp

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,4 +5,9 @@ import json
 def get_response_data(route):
     with application.app.test_client() as client:
         response = client.get(route)
-    return json.loads(response.get_data(as_text=True))
+    return response.get_data(as_text=True)
+
+
+def get_response_json(route):
+    data = get_response_data(route)
+    return json.loads(data)
--- tests/test_sra.py
+++ tests/test_sra.py
@@ -1,6 +1,6 @@
-from . import get_response_data
+from . import get_response_json
 
 
 def test_analysis_index_route():
-    data = get_response_data("/index/run=ERR2756788")
+    data = get_response_json("/index/run=ERR2756788")
     assert data == {'run_id': 'ERR2756788', 'srarun': True, 'nsra': True, 'psra': True, 'rsra': True, 'assembly': True, 'micro': True, 'geo': False}
