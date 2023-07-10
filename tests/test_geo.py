70ed697765d23393b5ebb89c7e7b78bc2f82d248
Al Mo <almosnow@gmail.com>
2023-07-10 18:08:32 +0000

    test: test_geo.py

--- tests/test_geo.py
+++ tests/test_geo.py
@@ -1,34 +1,33 @@
 from . import  get_response_json
 
-def test_dummy():
-  print('...')
+def test_paginate_geo():
+    first_page = get_response_json("/geo/rdrp/paged?page=1&perPage=10")
+    assert len(first_page['result']) == 10
+    
+    assert 'run_id' in first_page['result'][0]
+    assert isinstance(first_page['result'][0]['run_id'], str)
+    assert 'biosample_id' in first_page['result'][0]
+    assert isinstance(first_page['result'][0]['biosample_id'], str)
+    assert 'release_date' in first_page['result'][0]
+    assert isinstance(first_page['result'][0]['release_date'], str)
+    assert 'tax_id' in first_page['result'][0]
+    assert isinstance(first_page['result'][0]['tax_id'], str)
+    assert 'scientific_name' in first_page['result'][0]
+    assert isinstance(first_page['result'][0]['scientific_name'], str)
+    assert 'coordinate_x' in first_page['result'][0]
+    assert isinstance(first_page['result'][0]['coordinate_x'], float)
+    assert 'coordinate_y' in first_page['result'][0]
+    assert isinstance(first_page['result'][0]['coordinate_y'], float)
+    assert 'from_text' in first_page['result'][0]
+    assert isinstance(first_page['result'][0]['from_text'], str)
 
-# def test_paginate_geo():
-#     first_page = get_response_json("/geo/rdrp/paged?page=1&perPage=10")
-#     assert len(first_page['result']) == 10
-#     assert first_page['result'][0] == {
-#       "run_id": "DRR021440",
-#       "biosample_id": "SAMD00018407",
-#       "release_date": "Tue, 14 Jul 2015 10:38:15 GMT",
-#       "tax_id": "318829",
-#       "scientific_name": "Pyricularia oryzae",
-#       "coordinate_x": -53.073466889,
-#       "coordinate_y": -10.769946429,
-#       "from_text": "brazil"
-#     }
-
-#     sixth_page = get_response_json("/geo/rdrp/paged?page=6&perPage=5")
-#     assert len(sixth_page['result']) == 5
-#     assert sixth_page['result'][0] == {
-#         "run_id":	"DRR029069",
-#         "biosample_id":	"SAMD00024757",
-#         "release_date":	"Mon, 26 Jan 2015 10:28:36 GMT",
-#         "tax_id":	"64793",
-#         "scientific_name":	"Wasmannia auropunctata",
-#         "coordinate_x":	-52.97901,
-#         "coordinate_y":	5.067,
-#         "from_text":	None,
-#     }
+    second_page = get_response_json("/geo/rdrp/paged?page=2&perPage=5")
+    
+    assert first_page['result'][5] == second_page['result'][0]
+    assert first_page['result'][6] == second_page['result'][1]
+    assert first_page['result'][7] == second_page['result'][2]
+    assert first_page['result'][8] == second_page['result'][3]
+    assert first_page['result'][9] == second_page['result'][4]
 
 # def test_geo_run_filter():
 #     expected = {
