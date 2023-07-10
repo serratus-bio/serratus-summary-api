c3fda8714f35d52bb167e20d9f3496c4c4aee903
Al Mo <almosnow@gmail.com>
2023-07-10 19:22:53 +0000

    chore: removed dead code

--- tests/test_geo.py
+++ tests/test_geo.py
@@ -26,29 +26,6 @@ def test_paginate_geo():
     assert first_page['result'][9] == second_page['result'][4]
 
 def test_geo_run_filter():
-    expected = {
-          "DRR021440" : {
-            "run_id":	"DRR021440",
-            "biosample_id":	"SAMD00018407",
-            "release_date":	"Tue, 14 Jul 2015 10:38:15 GMT",
-            "tax_id":	"318829",
-            "scientific_name":	"Pyricularia oryzae",
-            "coordinate_x":	-53.073466889,
-            "coordinate_y":	-10.769946429,
-            "from_text":	"brazil",
-          },
-        "SRR13187411": {
-            "run_id":	"SRR13187411",
-            "biosample_id":	"SAMN16984707",
-            "release_date":	"Wed, 02 Dec 2020 19:04:46 GMT",
-            "tax_id":	"2697049",
-            "scientific_name":	"Severe acute respiratory syndrome coronavirus 2",
-            "coordinate_x":	-91.0969,
-            "coordinate_y":	30.5694,
-            "from_text":	None
-        }
-    }
-
     single_run = get_response_json("/geo/rdrp/paged?run=DRR021440")
     assert single_run['result'][0]['run_id'] == 'DRR021440'
 
