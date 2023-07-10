b7472319f28b392a12f24bd8397a3fb617ba1692
Al Mo <almosnow@gmail.com>
2023-07-10 20:12:25 +0000

    test: test_nucleotide.py

--- tests/test_geo.py
+++ tests/test_geo.py
@@ -5,14 +5,14 @@ def test_paginate_geo():
     assert len(first_page['result']) == 10
 
     for [key, type] in [
-        ['run_id', str],
         ['biosample_id', str],
-        ['release_date', str],
-        ['tax_id', str],
-        ['scientific_name', str],
         ['coordinate_x', float],
         ['coordinate_y', float],
-        ['from_text', str]
+        ['from_text', str],
+        ['release_date', str],
+        ['run_id', str],
+        ['scientific_name', str],
+        ['tax_id', str]
     ]:
       assert key in first_page['result'][0]
       assert isinstance(first_page['result'][0][key], type)
