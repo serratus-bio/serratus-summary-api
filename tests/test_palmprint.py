b5038f88dfc7c006da57bb578875ec1548a2fd8a
Al Mo <almosnow@gmail.com>
2023-07-11 14:46:41 +0000

    test: test_palmprint.py

---
+++ tests/test_palmprint.py
@@ -0,0 +1,25 @@
+from . import get_response_json
+
+def test_list():
+    result = get_response_json("/palmprint/run=ERR2756788")
+
+    for [key, type] in [
+        ['assembly_node', int],
+        ['cigar', str],
+        ['coverage', float],
+        ['evalue': float],
+        ['palm_id', str],
+        ['percent_identity', int],
+        ['pp_end', int],
+        ['pp_start', int],
+        ['q_end', int],
+        ['q_len', int],
+        ['q_sequence', str],
+        ['q_start', int],
+        ['q_strand', str],
+        ['qc_pass', bool],
+        ['row_id', int],
+        ['run_id', str]
+    ]:
+      assert key in result['result'][0]
+      assert isinstance(result['result'][0][key], type)
